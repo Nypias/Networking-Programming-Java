b4c88c2950c99d746646bc2908501ca498fb303e
teo <teo@teolaptopu>
2012-10-31 15:56:41 +0100

    Added thread to handle connectivity

--- HW1/src/Client/Client.java
+++ HW1/src/Client/Client.java
@@ -10,55 +10,55 @@ import java.util.regex.Pattern;
 
 import javax.swing.JOptionPane;
 
-public class Client {
+public class Client implements Runnable {
 
     //Connection variables
     private BufferedReader in = null;
     private Socket clientSocket = null;
     private DataOutputStream out = null;
     private MyGUI gui = null;
-    
     //Game Parameters
     private String word = "";
     private String tries = "";
-    private String score="";
+    private String score = "";
+
     /**
      * Constructor of Client class
+     *
      * @param host Host (IP) of the server
      * @param port Port of the server listener socket
      */
     public Client(String host, int port) {
         try {
-        	// Initialization of the client socket and input and output communication
+            // Initialization of the client socket and input and output communication
             clientSocket = new Socket(host, port);
             in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
             out = new DataOutputStream(clientSocket.getOutputStream());
         } catch (UnknownHostException ex) {
-        	System.out.println("Host unknown");
+            System.out.println("Host unknown");
             ex.printStackTrace();
         } catch (IOException ex) {
-        	System.out.println("Communication input/output not created");
+            System.out.println("Communication input/output not created");
             ex.printStackTrace();
         }
     }
 
     /**
-     * ConnectGUI
-     * Display the graphical user interface
+     * ConnectGUI Display the graphical user interface
      */
     void connectGUI() {
         gui = new MyGUI(this);
-        Thread t = new Thread(gui);
-        t.start();
+        Thread guiThread = new Thread(gui);
+        guiThread.start();
     }
 
     /**
-     * recvMessage
-     * Receive a new message
+     * recvMessage Receive a new message
      */
     protected void recvMessage() {
         //Read from in stream
-        String line = "";
+        
+        String line;
         Pattern patt = Pattern.compile("|", Pattern.LITERAL);
         try {
             while (true) {
@@ -84,7 +84,7 @@ public class Client {
                         gui.getTriesLabel().setText("Tries: " + tries);
                         gui.getWordLabel().setText(word);
                         gui.getScoreLabel().setText("Score: " + score);
-                        JOptionPane.showMessageDialog(gui.getF(),"You Won! Start a new Game", "Hangman Result", JOptionPane.PLAIN_MESSAGE);
+                        JOptionPane.showMessageDialog(gui.getF(), "You Won! Start a new Game", "Hangman Result", JOptionPane.PLAIN_MESSAGE);
 
                         //terminate();
                         //break;
@@ -95,22 +95,20 @@ public class Client {
                         tries = patt.split(line)[2];
                         gui.getWordLabel().setText(word);
                         gui.getTriesLabel().setText("Tries: " + tries);
-                        JOptionPane.showMessageDialog(gui.getF(),"You Lost! Start a new Game", "Hangman Result", JOptionPane.PLAIN_MESSAGE);
+                        JOptionPane.showMessageDialog(gui.getF(), "You Lost! Start a new Game", "Hangman Result", JOptionPane.PLAIN_MESSAGE);
                         //terminate();
                         //break;
-
                     }
                 }
             }
         } catch (IOException ex) {
             ex.printStackTrace();
         }
-
-
     }
 
     /**
      * SendMessage
+     *
      * @param message
      */
     protected void sendMessage(String message) {
@@ -127,33 +125,40 @@ public class Client {
 
     protected void terminate() {
         try {
-            //TODO: close streams
-            clientSocket.close();            
+            //TODO: maybe close streams
+            clientSocket.close();
         } catch (IOException ex) {
-            
         }
-
-
+    }
+    
+    @Override
+    public void run() {
+        recvMessage();
     }
 
     public static void main(String argv[]) throws Exception {
-    	
-    	String ip = "127.0.0.1";
-    	int port = 9000;
-    	
-    	// Capture port and server ip from arguments
+
+        String ip = "127.0.0.1";
+        int port = 9000;
+
+        // Capture port and server ip from arguments
         try {
-    		if (argv.length >1) port = Integer.parseInt(argv[1]);
-    		if (argv.length >0) ip = argv[0];
-    	} catch (NumberFormatException e) {
-    		System.out.println("USAGE: java Client [ip] [port]");
-    		System.exit(1);
-    	}
-    	    	
+            if (argv.length > 1) {
+                port = Integer.parseInt(argv[1]);
+            }
+            if (argv.length > 0) {
+                ip = argv[0];
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("USAGE: java Client [ip] [port]");
+            System.exit(1);
+        }
+
         // Begin the client communication
         Client client = new Client(ip, port);
-
         client.connectGUI();
-        client.recvMessage();
+        Thread clientThread = new Thread(client);
+        clientThread.start();
+
     }
 }
